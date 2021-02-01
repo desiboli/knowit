6efd212d57e58c9aabcb3765b9bed078f11c7c82
Mustafa Alshammaa <adam.benals@gmail.com>
2021-02-01 18:47:03 +0100

    static site generate route fallback

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,13 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: true,
+  ssr: false,
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+
+  generate: {
+    fallback: true,
+  },
 
   loadingIndicator: {
     name: 'folding-cube',
