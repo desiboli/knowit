c2af767a6df65b1505bc3fe2dba3c5abad81da80
Mustafa Alshammaa <adam.benals@gmail.com>
2021-02-01 17:53:51 +0100

    universal

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,8 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  mode: 'universal',
+
   loadingIndicator: {
     name: 'folding-cube',
     color: '#3B8070',
